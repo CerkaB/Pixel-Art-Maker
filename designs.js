7a6cff98df2086f81e46b74a88e1e918dc979e67
CerkaB <35803217+CerkaB@users.noreply.github.com>
2018-01-29 16:36:02 +0000

    Version 1.0
    This is the first version

---
+++ designs.js
@@ -0,0 +1,34 @@
+//Welcome to Boris' Pixel Art Project
+//Version: V 1.0.0
+const table = $('#pixel_canvas');
+// Select color input
+const colorPicker = $('#colorPicker');
+// Select size input
+const tableHeight = $('#input_height');
+const tableWidth = $('#input_width');
+//================= makeGrid Function ===================//
+function makeGrid() {
+  for (let y = 0; y < tableHeight.val(); y++){
+    table.append("<tr></tr>");
+    for (let x = 0; x < tableWidth.val(); x++) {
+      table.children().last().append("<td></td>");
+    }
+  }
+}
+//================= Draw Function ===================//
+table.on("click" , "td" , function(){
+  $(this).css("background-color" , colorPicker.val());
+});
+//================= Submit/Clear Button ===================//
+$('form').submit(function(e) {
+  table.empty();
+  e.preventDefault();
+  makeGrid();
+});
+//================= Reset Button ===================//
+$('#reset').click(function() {
+  table.empty();
+  colorPicker.val("#000000");
+  tableHeight.val(30);
+  tableWidth.val(30);
+});
